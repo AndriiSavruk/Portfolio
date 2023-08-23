10f54fce09dfe54b31c8785227ed4a799eceeaf1
AndriiSavruk <plaf@ukr.net>
2023-08-23 11:56:04 +0300

    Update KapustaAppE2ETesting.cy.js

--- Cypress Samples/cypress/e2e/KapustaAppE2ETesting.cy.js
+++ Cypress Samples/cypress/e2e/KapustaAppE2ETesting.cy.js
@@ -1,3 +1,4 @@
+
 describe('E2E Testing Kapusta App', () => {
     beforeEach("Log in", () => {
         cy.fixture("kapustaUser").then( (data) => {
@@ -9,7 +10,19 @@ describe('E2E Testing Kapusta App', () => {
         })
     })
 
-    it("Add 100 USD test", () => {
+    it("Buy smth for 50 USD and get 50 USD", () => {
+        // cy.get('.FormTransaction_input__Jbj3O').type('Bought some fruits').should('have.text','Bought some fruits');
+        // cy.get('.css-ackcql').select('Products').should('have.value','Products');
+        // cy.get('.FormTransaction_calcInput__cByYk').type('50').should('have.value','50');
+        // cy.get('.FormTransaction_buttonInput__Fqdxq').click();
+
+        // cy.wait(2000);
+        // cy.get('.HomeNavigation_link__qGIjl').click();
+        // cy.get('.FormTransaction_input__Jbj3O').type('Bank interests').should('have.text','Bank interests');
+        // cy.get('.FormTransaction_input__Jbj3O').select('Add. income').should('have.value','Add. income');
+        // cy.get('.FormTransaction_calcInput__cByYk').type('50').should('have.value','50');
+        // cy.get('.FormTransaction_buttonInput__Fqdxq').click();
+        
         cy.get('.Balance_input__5zeug').should('have.value','100.00');
     })
 })
