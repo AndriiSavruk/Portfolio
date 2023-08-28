1875dfa02a4238cb60029df887138ad2860246fc
AndriiSavruk <plaf@ukr.net>
2023-08-28 12:11:55 +0300

    Update KapustaAppE2ETesting.cy.js

--- Cypress Samples/cypress/e2e/KapustaAppE2ETesting.cy.js
+++ Cypress Samples/cypress/e2e/KapustaAppE2ETesting.cy.js
@@ -11,22 +11,22 @@ describe('E2E Testing Kapusta App', () => {
     })
 
     it("Buy smth for 50 USD and get 50 USD", () => {
-        cy.get('.HomeNavigation_activeLink__2oES1').click();
-        cy.get('.FormTransaction_input__Jbj3O').type('Bought some fruits');
-        // .should('have.text','Bought some fruits');
-        cy.get('.css-ackcql').type('Products');
+        // cy.get('.HomeNavigation_activeLink__2oES1').click();
+        // cy.get('.FormTransaction_input__Jbj3O').clear().type('Bought some fruits')
+        // .should('have.value','Bought some fruits');
+        // cy.get('.css-ackcql').clear().type('Products')
         // .should('have.value','Products');
-        cy.get('.FormTransaction_calcInput__cByYk').type('50').should('have.value','50');
-        cy.get('.FormTransaction_buttonInput__Fqdxq').click();
+        // cy.get('.FormTransaction_calcInput__cByYk').type('50').should('have.value','50');
+        // cy.get('.FormTransaction_buttonInput__Fqdxq').click();
 
-        cy.wait(2000);
-        cy.get('.HomeNavigation_link__qGIjl').click();
-        cy.get('.FormTransaction_input__Jbj3O').type('Bank interests');
-        // .should('have.text','Bank interests');
-        cy.get('.FormTransaction_input__Jbj3O').type('Add. income');
-        // .should('have.value','Add. income');
-        cy.get('.FormTransaction_calcInput__cByYk').type('50').should('have.value','50');
-        cy.get('.FormTransaction_buttonInput__Fqdxq').click();
+        // cy.wait(2000);
+        // cy.get('.HomeNavigation_link__qGIjl').click();
+        // cy.get('.FormTransaction_input__Jbj3O').type('Bank interests')
+        // .should('have.value','Bank interests');
+        // // cy.get('.css-tlfecz-indicatorContainer').select('Add. income');
+        // // .should('have.value','Add. income');
+        // cy.get('.FormTransaction_calcInput__cByYk').type('50').should('have.value','50');
+        // cy.get('.FormTransaction_buttonInput__Fqdxq').click();
         
         cy.get('.Balance_input__5zeug').should('have.value','100.00');
     })
